36aa29e348ba521990b36aea3605e09383fbdc37
Calamari <georg@tavonius.de>
2020-02-25 11:53:16 +0100

    Add page aliases because of old zouk-hh.de

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
-#Bootstrap is used to style bits of the demo. Remove it from the config, gemfile and stylesheets to stop using bootstrap
-require "uglifier"
+# frozen_string_literal: true
+
+# Bootstrap is used to style bits of the demo. Remove it from the config, gemfile and stylesheets to stop using bootstrap
+require 'uglifier'
 
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
@@ -10,8 +12,10 @@ Slim::Engine.set_options shortcut: {
   '#' => { tag: 'div', attr: 'id' }, '.' => { tag: 'div', attr: 'class' }
 }
 
+activate :alias
+
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+  prefix.browsers = 'last 2 versions'
 end
 
 activate :livereload
@@ -22,13 +26,13 @@ activate :livereload
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-page "/partials/*", layout: false
-page "/admin/*", layout: false
+page '/partials/*', layout: false
+page '/admin/*', layout: false
 
 activate :blog do |blog|
-  blog.permalink = "news/{year}/{title}.html"
-  blog.sources = "posts/{title}.html"
-  blog.layout = "news-detail"
+  blog.permalink = 'news/{year}/{title}.html'
+  blog.sources = 'posts/{title}.html'
+  blog.layout = 'news-detail'
 end
 
 # With alternative layout
@@ -42,8 +46,8 @@ data.people.each do |_filename, person|
   # people is an array: [filename, {data}]
   proxy(
     "/das-sind-wir/#{person[:name].parameterize}/index.html",
-    "person.html",
-    locals: {person: person},
+    'person.html',
+    locals: { person: person },
     # layout: 'person-detail',
     ignore: true
   )
@@ -57,19 +61,19 @@ end
 activate :directory_indexes
 
 helpers do
-  #helper to set background images with asset hashes in a style attribute
+  # helper to set background images with asset hashes in a style attribute
   def background_image(image)
-    "background-image: url('" << image_path(image) << "')"
+    "background-image: url('" + image_path(image) + "')"
   end
 
   def nav_link(link_text, url, options = {})
-    options[:class] ||= ""
-    options[:class] << " active" if url == current_page.url
+    options[:class] ||= ''
+    options[:class] = options[:class] + ' active' if url == current_page.url
     link_to(link_text, url, options)
   end
 
   def markdown(content)
-     Tilt['markdown'].new { content }.render
+    Tilt['markdown'].new { content }.render
   end
 end
 
@@ -81,12 +85,11 @@ configure :build do
   activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript, ignore: "**/admin/**", compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: {comments: :none})
+  activate :minify_javascript, ignore: '**/admin/**', compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: { comments: :none })
 
   # Use Gzip
   activate :gzip
 
-  #Use asset hashes to use for caching
-  #activate :asset_hash
-
+  # Use asset hashes to use for caching
+  # activate :asset_hash
 end
